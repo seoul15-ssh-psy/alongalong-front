78be7dc0dd16a2753ee455b3b2f9a5b209b2b4a6
sypark <soun997@gmail.com>
2023-05-16 03:17:28 +0900

    feat: 컨텐츠 title 컴포넌트 구현

---
+++ src/components/main/MainHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row q-mt-md">
+    <a>
+      <div class="wrapper">
+        <slot name="header-icon"></slot>
+        <div class="text-h4 text-bold text-teal-5">
+          <slot name="header-title"></slot>
+        </div>
+        <q-separator
+          class="q-ml-md q-mr-md"
+          vertical
+          color="teal-1"
+          size="3px"
+        />
+        <div class="text-h6 text-bold text-teal-6">
+          <slot name="header-text"></slot>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {}
+}
+</script>
+
+<style>
+.wrapper {
+  display: flex;
+  align-items: center;
+}
+</style>
