9e183363ce518400e02f3706dd2aab18e7734914
ssh102525@naver.com <ssh102525@naver.com>
2023-05-15 14:50:26 +0900

    feat/리스트 넘어갈 때 텍스트 변경 추가

--- src/components/UpperIntro/UpperIntro.vue
+++ src/components/UpperIntro/UpperIntro.vue
@@ -1,11 +1,10 @@
 <template>
     
-    <div class="bg-primary gt-xs" id="upperParent">
+    <div class="bg-primary gt-xs" id="upperParent1">
         <!-- 배경 색깔 + 텍스트-->
         <div class="bg-positive" id="upperColor">
-            <p class="upperText1" :style="{fontSize:upperText1Size+'px'}">밤 나들이</p>
-            <p class="upperText1" :style="{fontSize:upperText1Size+'px'}">야간 개장으로 만나는</p>
-            <p class="upperText1" :style="{fontSize:upperText1Size+'px'}">고즈넉~</p>
+            <p class="upperText1" :style="{fontSize:upperText1Size+'px'}" v-html="slideText" ></p>
+
         </div>
         <!-- 하단 숫자 01/05 & 재생, 앞으로 가기, 뒤로 가기-->
         <div class="row" id="upperBar">
@@ -27,19 +26,18 @@
             class=" rounded-borders"
             id = "upperImgList"
             >
-            <q-carousel-slide name="style">
+            <q-carousel-slide name="imgList1">
                 <img src="../../../public/icons/mainIcon1.png" id="img">
             </q-carousel-slide>
-            <q-carousel-slide name="tv">
+            <q-carousel-slide name="imgList2">
                 <img src="../../../public/icons/mainIcon2.png" id="img">
             </q-carousel-slide>
         </q-carousel>
         
     </div>
 
-    <div class="bg-primary lt-sm" id="upperParent">
+    <div class="bg-primary lt-sm" id="upperParent2">
         <div id="imgArea">
-
             <q-carousel
             v-model="slide"
             transition-prev="slide-right"
@@ -48,17 +46,15 @@
             class="rounded-borders"
             id = "upperImgList2"
             >
-            <q-carousel-slide name="style">
-                <img src="../../../public/icons/mainIcon1.png" id="img2">
+                <q-carousel-slide name="imgList1">
+                    <img src="../../../public/icons/mainIcon1.png" id="img2">
                 </q-carousel-slide>
-                <q-carousel-slide name="tv">
+                <q-carousel-slide name="imgList2">
                     <img src="../../../public/icons/mainIcon2.png" id="img2">
                 </q-carousel-slide>
             </q-carousel>
             <q-linear-progress :value="progress" size="5px" color="secondary" class="q-mt-sm" instant-feedback="true" animation-speed="500"/>
-            <p class="upperText2">밤 나들이</p>
-            <p class="upperText2">야간 개장으로 만나는</p>
-            <p class="upperText2">고즈넉~</p>
+            <p class="upperText2" v-html="slideText"></p>
         </div>
     </div>
 
@@ -73,7 +69,8 @@ export default {
     
     data() { 
         return {
-            slide: ref('style'),
+            slide: ref('imgList1'),
+            slideText:"밤 나들이 <br> 야간 개장으로 만나는<br> 고즈넉~",
             progress: 0,
             interval: 0,
             upperText1Size: 30,
@@ -86,7 +83,10 @@ export default {
     },
    
     mounted() {
-        let slideList = [ref('style'),ref('tv')];
+        let slideList = [ref('imgList1'), ref('imgList2')];
+        let upperTextContent = ["밤 나들이 <br>야간 개장으로 만나는 <br>고즈넉~","여기는<br> 또 어떤 느낌의 <br>여행지일까요 ㅎㅎ"];
+        
+        
         this.interval = setInterval(() => {
             if (this.toggleOn) { 
                 this.progress = this.progress + 0.01;
@@ -95,6 +95,7 @@ export default {
                 this.progress = -0.1;
                 this.i = this.i == 0 ? this.i = this.max-1 : this.i - 1;
                 this.slide = slideList[this.i];
+                this.slideText = upperTextContent[this.i];
                 this.goLeft = false;
                 return;
             }
@@ -102,6 +103,7 @@ export default {
                 this.progress = -0.1;
                 this.i = (this.i+1) % 2;
                 this.slide = slideList[this.i];
+                this.slideText = upperTextContent[this.i];
                 this.goRight = false;
                 return;
             }
@@ -110,6 +112,7 @@ export default {
                 //여기서 사진 옆으로 돌리기
                 this.i = (this.i+1) % 2;
                 this.slide = slideList[this.i];
+                this.slideText = upperTextContent[this.i];
                 return;
             }
         }, 100);
@@ -143,9 +146,13 @@ export default {
 </script>
 
 <style scoped>
-#upperParent{
+#upperParent1{
     height:60% !important;
     padding-bottom:50px;
+}
+#upperParent2{
+    height:60% !important;
+    padding-bottom:10px;
 }
 #upperColor{
     padding-top: 16%;
@@ -187,6 +194,7 @@ export default {
 .upperText2{
     font-size:1.3em;
     margin: 0;
+    margin-top: 10px;
     font-weight: 600;
 }
 
