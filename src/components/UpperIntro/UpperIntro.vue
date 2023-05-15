c1d50c7589678c2e561ecadbe025d32cd32339d8
ssh102525@naver.com <ssh102525@naver.com>
2023-05-15 17:45:16 +0900

    feat/배경색 변경/요소 크기 변경, 애니메이션  속도 추가, 화면 크기에 따른 Padding 요소크기 상세 설정,이미지 변경 시  텍스트 변경

--- src/components/UpperIntro/UpperIntro.vue
+++ src/components/UpperIntro/UpperIntro.vue
@@ -1,13 +1,12 @@
 <template>
     
-    <div class="bg-primary gt-xs" id="upperParent1">
+    <div class=" gt-xs" id="upperParent1">
         <!-- 배경 색깔 + 텍스트-->
-        <div class="bg-positive" id="upperColor">
-            <p class="upperText1" :style="{fontSize:upperText1Size+'px'}" v-html="slideText" ></p>
-
+        <div class="moveArea1" :style="[{backgroundColor:bgColor },{paddingLeft:upperColorPL+'px'},{paddingBottom:upperColorPB+'px'}]" id="upperColor">
+            <p class="upperText1 moveArea2" :style="{fontSize:upperText1Size+'px'}" v-html="slideText" ></p>
         </div>
         <!-- 하단 숫자 01/05 & 재생, 앞으로 가기, 뒤로 가기-->
-        <div class="row" id="upperBar">
+        <div class="row" id="upperBar" :style="{paddingLeft:upperColorPL+'px'}">
             <div class="col-3 row ">
                 <q-linear-progress :value="progress" size="5px" color="secondary" class="q-mt-sm" instant-feedback="true" animation-speed="500"/>
             </div>
@@ -23,34 +22,42 @@
             transition-prev="slide-right"
             transition-next="slide-left"
             animated
-            class=" rounded-borders"
+            padding
             id = "upperImgList"
+            :style="{width:imgWidth+'px'}"
+            class="transparent moveArea2"
             >
-            <q-carousel-slide name="imgList1">
-                <img src="../../../public/icons/mainIcon1.png" id="img">
+            <q-carousel-slide name="imgList1" class="transparent padding-0 ">
+                <img src="../../../public/attraction/upperIntro/skyrim(1).jpg" class="img rounded-borders">
             </q-carousel-slide>
-            <q-carousel-slide name="imgList2">
-                <img src="../../../public/icons/mainIcon2.png" id="img">
+            <q-carousel-slide name="imgList2" class="transparent  padding-0">
+                <img src="../../../public/attraction/upperIntro/skyrim(2).jpg" class="img rounded-borders">
+            </q-carousel-slide>
+            <q-carousel-slide name="imgList3" class="transparent  padding-0">
+                <img src="../../../public/attraction/upperIntro/skyrim(3).jpg" class="img rounded-borders">
             </q-carousel-slide>
         </q-carousel>
         
     </div>
 
-    <div class="bg-primary lt-sm" id="upperParent2">
+    <div class="lt-sm" :style="{backgroundColor:bgColor}"  id="upperParent2">
         <div id="imgArea">
             <q-carousel
             v-model="slide"
             transition-prev="slide-right"
             transition-next="slide-left"
             animated
-            class="rounded-borders"
             id = "upperImgList2"
+            class="transparent"
             >
-                <q-carousel-slide name="imgList1">
-                    <img src="../../../public/icons/mainIcon1.png" id="img2">
+                <q-carousel-slide name="imgList1" class="transparent padding-0">
+                    <img src="../../../public/attraction/upperIntro/skyrim(1).jpg" class="img rounded-borders">
                 </q-carousel-slide>
-                <q-carousel-slide name="imgList2">
-                    <img src="../../../public/icons/mainIcon2.png" id="img2">
+                <q-carousel-slide name="imgList2" class="transparent padding-0">
+                    <img src="../../../public/attraction/upperIntro/skyrim(2).jpg" class="img rounded-borders">
+                </q-carousel-slide>
+                <q-carousel-slide name="imgList3" class="transparent padding-0">
+                    <img src="../../../public/attraction/upperIntro/skyrim(3).jpg" class="img rounded-borders">
                 </q-carousel-slide>
             </q-carousel>
             <q-linear-progress :value="progress" size="5px" color="secondary" class="q-mt-sm" instant-feedback="true" animation-speed="500"/>
@@ -75,17 +82,29 @@ export default {
             interval: 0,
             upperText1Size: 30,
             i: 0,
-            max: 2,
+            max: 3,
             toggleOn: true,
             goLeft: false,
-            goRIght: false,
+            goRight: false,
+            bgColor: '#FDC4F8',
+            imgWidth:100,
+            upperColorPL:100,
+            upperColorPB:100,
         };
+    },
+    created(){ 
+        const screenWidth = window.innerWidth
+        this.upperText1Size = screenWidth;
+        this.upperText1Size = this.upperText1Size / 60 + 10;
+            this.imgWidth = screenWidth / 3 + 100;
+        this.upperColorPL = screenWidth * screenWidth / 10000;
+        this.upperColorPB = (screenWidth / 10) - 50;
     },
    
     mounted() {
-        let slideList = [ref('imgList1'), ref('imgList2')];
-        let upperTextContent = ["밤 나들이 <br>야간 개장으로 만나는 <br>고즈넉~","여기는<br> 또 어떤 느낌의 <br>여행지일까요 ㅎㅎ"];
-        
+        let slideList = [ref('imgList1'), ref('imgList2'), ref('imgList3')];
+        let upperTextContent = ["밤 나들이 <br>야간 개장으로 만나는 <br>고즈넉~","여기는<br> 또 어떤 느낌의 <br>여행지일까요 ㅎㅎ","여기는<br> 스카이림 <br>노드의 땅"];
+        let bgColorList = ['#FDC4F8','#CB9FFD','#BAE7AF'];
         
         this.interval = setInterval(() => {
             if (this.toggleOn) { 
@@ -96,23 +115,26 @@ export default {
                 this.i = this.i == 0 ? this.i = this.max-1 : this.i - 1;
                 this.slide = slideList[this.i];
                 this.slideText = upperTextContent[this.i];
+                this.bgColor = bgColorList[this.i];
                 this.goLeft = false;
                 return;
             }
             if (this.goRight) { 
                 this.progress = -0.1;
-                this.i = (this.i+1) % 2;
+                this.i = (this.i+1) % this.max;
                 this.slide = slideList[this.i];
                 this.slideText = upperTextContent[this.i];
+                this.bgColor = bgColorList[this.i];
                 this.goRight = false;
                 return;
             }
             if (this.progress >= 1) {
                 this.progress = -0.1;
                 //여기서 사진 옆으로 돌리기
-                this.i = (this.i+1) % 2;
+                this.i = (this.i+1) % this.max;
                 this.slide = slideList[this.i];
                 this.slideText = upperTextContent[this.i];
+                this.bgColor = bgColorList[this.i];
                 return;
             }
         }, 100);
@@ -125,7 +147,10 @@ export default {
         onResize: function(event){ 
             const screenWidth = window.innerWidth
             this.upperText1Size = screenWidth;
-            this.upperText1Size = this.upperText1Size / 40 + 10;
+            this.upperText1Size = this.upperText1Size / 60 + 10;
+            this.imgWidth = screenWidth / 3 + 100;
+            this.upperColorPL = screenWidth * screenWidth / 10000;
+            this.upperColorPB = (screenWidth / 10) - 50;
         },
         imgListLeft: function (event) { 
             this.goLeft = true;
@@ -155,22 +180,17 @@ export default {
     padding-bottom:10px;
 }
 #upperColor{
-    padding-top: 16%;
-    padding-bottom:4%;
-    padding-left:15%;
+    padding-top: 180px;
+    padding-left:5%;
 }
 #upperBar{
     padding-top: 4%;
     padding-left:15%;
     height: 10px;
 }
-#img{
+.img{
     height:auto;
-    width:100%;
-}
-#img2{
-    height:auto;
-    width:100%;
+    width:100% !important;
 }
 #imgArea{
     margin-left:2%;
@@ -183,10 +203,9 @@ export default {
     margin-top:110px;
     z-index: 2;
     position: absolute;
-    width: 40%;
+    padding:0px !important;
     height : auto;
 }
-
 .upperText1{
     margin: 0;
     font-weight: 600;
@@ -197,6 +216,14 @@ export default {
     margin-top: 10px;
     font-weight: 600;
 }
-
+.moveArea1{
+    transition: 1s background ease;
+}
+.moveArea2{
+    transition: 0.1s ease;
+}
+.padding-0{
+    padding: 0;
+}
 
 </style>
