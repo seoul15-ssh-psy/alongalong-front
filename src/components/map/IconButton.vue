f9cb258f735195c30ced2825d05ec221609f7748
soun997 <soun997@gmail.com>
2023-05-17 17:45:42 +0900

    feat: IconButton 컴포넌트 구현(아이콘 버튼)

---
+++ src/components/map/IconButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <q-btn
+      :class="bgColor + ' q-mb-sm q-pa-sm'"
+      flat
+      round
+      color="white"
+      style="
+        border-style: solid;
+        border-width: 2px;
+        border-color: rgb(231, 231, 231);
+      "
+    >
+      <q-icon :name="iconName" size="35px" />
+    </q-btn>
+    <div class="text-center">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      required: true
+    },
+    bgColor: {
+      type: String,
+      default: 'bg-black-1'
+    },
+    title: {
+      type: String,
+      default: 'default title'
+    }
+  }
+}
+</script>
+
+<style></style>
