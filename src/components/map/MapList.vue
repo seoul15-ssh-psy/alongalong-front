b857044f7729a4d9783d5ca8f0937e5a92ef6cee
soun997 <soun997@gmail.com>
2023-05-17 17:47:14 +0900

    feat: 여행지 정보를 담는 scroll-area 컴포넌트 구현

---
+++ src/components/map/MapList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="items-stretch" style="width: 100%">
+    <q-scroll-area
+      style="height: 70vh; overflow-y: hidden"
+      :bar-style="{
+        borderRadius: '5px',
+        background: 'grey',
+        width: '7px'
+      }"
+      :thumb-style="{
+        borderRadius: '5px',
+        background: 'grey',
+        width: '7px',
+        opacity: 1
+      }"
+    >
+      <attraction-card></attraction-card>
+      <attraction-card></attraction-card>
+      <attraction-card></attraction-card>
+      <attraction-card></attraction-card>
+      <attraction-card></attraction-card>
+      <attraction-card></attraction-card>
+      <attraction-card></attraction-card>
+      <attraction-card></attraction-card>
+    </q-scroll-area>
+  </div>
+</template>
+<script>
+import AttractionCard from './AttractionCard.vue'
+export default {
+  components: {
+    AttractionCard
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
