3e074582de9c99d62518dd362429669409607c51
soun997 <soun997@gmail.com>
2023-05-17 17:47:29 +0900

    feat: 여행지 정보 검색 drawer 컴포넌트 구현

--- src/components/map/MapDrawer.vue
+++ src/components/map/MapDrawer.vue
@@ -3,21 +3,35 @@
     v-model="drawer"
     show-if-above
     :mini="!drawer || miniState"
-    :width="400"
-    :mini-width="50"
+    :width="450"
+    :mini-width="0"
     :breakpoint="300"
     bordered
     class="shadow-3"
   >
     <!-- contents -->
-    <div class="q-mini-drawer-hide text-h5 text-bold q-mx-lg q-my-xl">
-      서울특별시 관악구
+    <div class="q-mini-drawer-hide">
+      <div class="text-h5 text-bold q-mx-lg q-my-xl">
+        서울특별시 관악구
+        <q-separator class="q-mt-xs" color="grey-6" size="2px" />
+      </div>
+      <div class="row justify-center">
+        <div class="flex q-gutter-lg">
+          <icon-button
+            v-for="(item, index) in iconButtons"
+            :iconName="item.iconName"
+            :bgColor="item.bgColor"
+            :title="item.title"
+            :key="index"
+          />
+        </div>
+      </div>
+      <q-separator class="q-my-lg" />
+      <div class="row"><map-list></map-list></div>
     </div>
+
     <!-- drawer buttons -->
-    <div
-      class="flex absolute-right justify-end items-center"
-      style="height: 95vh"
-    >
+    <div class="flex absolute-right items-center">
       <div class="q-mini-drawer-only">
         <q-btn
           dense
@@ -26,6 +40,7 @@
           icon="arrow_forward_ios"
           size="20px"
           @click="miniState = false"
+          style="right: -100%"
         />
       </div>
       <div class="q-mini-drawer-hide">
@@ -36,6 +51,7 @@
           icon="arrow_back_ios"
           size="20px"
           @click="miniState = true"
+          style="right: -100%"
         />
       </div>
     </div>
@@ -44,7 +60,16 @@
 
 <script>
 import { ref } from 'vue'
+import IconButton from './IconButton.vue'
+import AttractionCard from './AttractionCard.vue'
+import MapList from './MapList.vue'
+
 export default {
+  components: {
+    IconButton,
+    MapList,
+    AttractionCard
+  },
   setup() {
     const miniState = ref(false)
 
@@ -57,16 +82,33 @@ export default {
           miniState.value = false
           e.stopPropagation()
         }
-      }
+      },
+
+      iconButtons: [
+        {
+          iconName: 'o_map',
+          bgColor: 'bg-green-3',
+          title: '관광지'
+        },
+        {
+          iconName: 'o_restaurant',
+          bgColor: 'bg-orange-3',
+          title: '음식점'
+        },
+        {
+          iconName: 'o_local_cafe',
+          bgColor: 'bg-pink-3',
+          title: '카페'
+        },
+        {
+          iconName: 'o_hotel',
+          bgColor: 'bg-purple-3',
+          title: '숙소'
+        }
+      ]
     }
   }
 }
 </script>
 
-<style>
-.edge {
-  border-top-right-radius: 10px;
-  border-bottom-right-radius: 10px;
-  background-color: grey;
-}
-</style>
+<style></style>
