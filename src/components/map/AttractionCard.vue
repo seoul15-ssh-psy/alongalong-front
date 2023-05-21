3770a15fe11b9be8f95081614cbe9a54a4a48c40
sypark <soun997@gmail.com>
2023-05-22 06:21:36 +0900

    style: 관광지명이 너무 길 경우 (...) 말줄임표로 생략

--- src/components/map/AttractionCard.vue
+++ src/components/map/AttractionCard.vue
@@ -2,6 +2,7 @@
   <div
     class="row items-center"
     style="
+    width: 100%
       height: 200px;
       border-style: solid;
       border-width: 1px;
@@ -9,17 +10,21 @@
     "
   >
     <div class="col-4">
-      <q-img
-        class="q-mx-sm"
-        :src="imageUrl"
-        :ratio="1"
-        style="border-radius: 5px"
-      ></q-img>
+      <a href="#"
+        ><q-img
+          class="q-ma-sm"
+          :src="imageUrl"
+          :ratio="1"
+          style="border-radius: 5px"
+        ></q-img
+      ></a>
     </div>
-    <div class="col-7 q-ml-lg">
+    <div class="col-7 q-ml-md">
       <div class="row items-center">
-        <div class="col text-subtitle1 text-bold">{{ title }}</div>
-        <div class="col text-subtitle2 q-ml-xs">{{ category }}</div>
+        <a href="#" class="subtitle1"
+          ><div class="text-subtitle1 text-bold">{{ title }}</div></a
+        >
+        <div class="subtitle2 text-subtitle2 q-ml-xs">{{ category }}</div>
       </div>
       <div class="row">
         <div class="text-subtitle2">내 위치로부터 {{ distance }}km</div>
@@ -57,4 +62,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.subtitle1 {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  max-width: 65%;
+  white-space: nowrap;
+}
+</style>
