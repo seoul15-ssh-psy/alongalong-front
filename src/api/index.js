1c2d384de48ed155faf8bbc8f309003607cc29cd
sypark <soun997@gmail.com>
2023-05-22 02:13:28 +0900

    feat: 한국관광공사, 카카오 API baseURL 등록

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,30 @@
-import axios from "axios";
+import axios from 'axios'
 
 // local vue api axios instance
 function apiInstance() {
   const instance = axios.create({
     baseURL: 'http://localhost:9999/vue',
     headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-  });
-  return instance;
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  })
+  return instance
 }
 
+function attractionApiInstance(latitude, longitude) {
+  const instance = axios.create({
+    baseURL: 'https://apis.data.go.kr/B551011/KorService1'
+  })
+  return instance
+}
 
+function kakaoMapApiInstance() {
+  const instance = axios.create({
+    baseURL: 'https://dapi.kakao.com/v2',
+    headers: {
+      Authorization: `KakaoAK ${process.env.KAKAO_APP_KEY}`
+    }
+  })
+}
 
-export { apiInstance};
+export { apiInstance, attractionApiInstance, kakaoMapApiInstance }
