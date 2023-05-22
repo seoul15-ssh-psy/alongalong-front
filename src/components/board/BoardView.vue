470cfec1df483c5944b2f6f18eee072d1c691daf
ssh102525@naver.com <ssh102525@naver.com>
2023-05-22 13:45:41 +0900

    feat: 파일 업로드/다운로드 및 이미지 나타내기 기능 추가

--- src/components/board/BoardView.vue
+++ src/components/board/BoardView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :style="{paddingTop:'100px'}" id="tables"> 
+  <div :style="{paddingTop:'100px'}" id="tableView"> 
   <table >
 		<tr>
 			<td>번호</td>
@@ -25,23 +25,23 @@
 			<td>내용</td>
 			<td><textarea rows="10" cols="50" name="content" v-model="article.content"></textarea></td>
 		</tr>
-	  
+		<img :src="'http://localhost:9999/vue/file/'+article.articleno" id="fileImg" style="display:none">
+		<a v-if="article.originalfile" :href="'http://localhost:9999/vue/file/download/'+article.savefolder+'/'+article.originalfile+'/'+article.savefile"><button >다운로드</button></a>
 	</table>
     <div id="buttons">
 			<button type="submit" @click="moveModifyArticle" v-if="checkSameUser()">수정하기</button>
 			<button type="submit" @click="deleteArticle" v-if="checkSameUser()">삭제하기</button>
 		    <button @click="moveList">목록보기</button>
   	</div>
-	
   </div>
 </template>
 
 <script>
-// import moment from "moment";
-import { getArticle, downloadFile } from "../../api/board";
+import { getArticle } from "../../api/board";
 import { mapState } from "vuex";
 
 const memberStore = "memberStore";
+const imgPrefix = ['jpg','png','JPG','PNG'];
 
 export default {
   name: "BoardDetail",
@@ -61,89 +61,82 @@ export default {
   },
   created() {
     let param = this.$route.params.articleno;
-    
+
     getArticle(
       param,
       ({ data }) => {
 		  this.article = data;
 		  this.article.regtime = this.convertTime(this.article.regtime);
-		  
-		  downloadFile(
-			this.article.articleno,
-			  ({ data }) => {
-				  console.log(typeof(data));
-				  let a = data;
-				  let reader = new FileReader();
-				  reader.onload = () => { 
-					  this.file = reader.result;
-				  }
-				  reader.readAsDataURL(a);
-				  console.log(data);
-			},
-			(error) => {
-				console.log(error);
-			  }
-			
-		);
+		  console.log("qjmeoqweopqwp"+this.article.originalfile.split('.')[1]);
+
+		  if (this.article.originalfile.split('.')[1] != undefined && imgPrefix.includes(this.article.originalfile.split('.')[1])) {
+			  document.getElementById("fileImg").style.display = 'block';
+			  console.log("나와라!");
+			  console.log(this.article.originalfile.split('.')[1]);
+		  } else { 
+			  console.log("사라져라!!");
+			  
+		  }
       },
       (error) => {
         console.log(error);
       }
 	  );
 	
-  },
+	},
   
-  methods: {
-	  moveModifyArticle() {
-		this.$router.push({
-			name: "boardmodify",
-			params: { articleno: this.article.articleno  },
-		});
-      },
-      deleteArticle() {
-		if (confirm("정말로 삭제?")) {
+	methods: {
+
+		moveModifyArticle() {
 			this.$router.push({
-			name: "boarddelete",
-			params: { articleno: this.article.articleno },
+				name: "boardmodify",
+				params: { articleno: this.article.articleno  },
 			});
+		},
+		deleteArticle() {
+			if (confirm("정말로 삭제?")) {
+				this.$router.push({
+				name: "boarddelete",
+				params: { articleno: this.article.articleno },
+				});
+			}
+		},
+		moveList() {
+			this.$router.push({ name: "boardlist" });
+		},
+		convertTime(regtime) { 
+			let time = new Date() - new Date(regtime);
+			//59분전
+			if (time < 3599999) {
+				return Math.floor(time / 60000) + " 분 전";
+			}
+			//23시간 전
+			else if (time < 82800000) {
+				return Math.floor(time / 3600000) + "시간 전";
+			}
+			//6일 전
+			else if (time < 604799999) {
+				return Math.floor(time / 86400000) + "일 전";
+			}
+			//? 주 전
+			else if (time < 2629799999) {
+				return Math.floor(time / 604800016) + "주 전";
+			}
+			//1년 전
+			else if (time < 31557599999) {
+				return Math.floor(time / 2629800000) + "개월 전";
+			}
+			else { 
+				return Math.floor(time / 31557600000) + "년 전";
+			}
+		},
+		checkSameUser() { 
+			if (this.userInfo!=null && this.article.userid == this.userInfo.userid) {
+				return true;
+			} else { 
+				return false;
+			}
 		}
-	  },
-      moveList() {
-        this.$router.push({ name: "boardlist" });
-	  },
-	  convertTime(regtime) { 
-		let time = new Date() - new Date(regtime);
-		//59분전
-		if (time < 3599999) {
-			return Math.floor(time / 60000) + " 분 전";
-		}
-		//23시간 전
-		else if (time < 82800000) {
-			return Math.floor(time / 3600000) + "시간 전";
-		}
-		//6일 전
-		else if (time < 604799999) {
-			return Math.floor(time / 86400000) + "일 전";
-		}
-		//? 주 전
-		else if (time < 2629799999) {
-			return Math.floor(time / 604800016) + "주 전";
-		}
-		//1년 전
-		else if (time < 31557599999) {
-			return Math.floor(time / 2629800000) + "개월 전";
-		}
-		else { 
-			return Math.floor(time / 31557600000) + "년 전";
-		}
-	  },
-	  checkSameUser() { 
-		  if (this.userInfo!=null && this.article.userid == this.userInfo.userid) {
-			  return true;
-		  } else { 
-			  return false;
-		  }
-	  }
   },
   // filters: {
   //   dateFormat(regtime) {
@@ -154,17 +147,17 @@ export default {
 </script>
 
 <style scope>
-#tables * {
+#tableView * {
 	text-align: center;
 	margin: auto;
 	font-family: 'Raleway', sans-serif;
 }
 
-#tables h1 {
+#tableView h1 {
 	margin: 50px auto;
 }
 
-#tables button {
+#tableView button {
 	border: 2px solid grey;
 	padding: 5px 8px;
 	border-radius: 3px;
@@ -175,22 +168,22 @@ export default {
 	border-radius: 3px;
 }
 
-#tables button:hover {
+#tableView button:hover {
 	background-color: black;
 	color: white;
 }
 
-#tables a {
+#tableView a {
 	color: black;
 	font-weight: bold;
 	text-decoration: none;
 }
 
-#tables p {
+#tableView p {
 	margin: 30px 0px;
 }
 
-#tables table {
+#tableView table {
 	background-color: whitesmoke;
 	margin: 15px auto;
 	border: 1px solid black;
@@ -198,16 +191,16 @@ export default {
 	font-size: 16px;
 }
 
-#tables td {
+#tableView td {
 	width: 400px;
 }
 
-#tables tr {
+#tableView tr {
 	height: 40px;
 	border: 2px solid black;
 }
 
-#tables td:nth-child(1) {
+#tableView td:nth-child(1) {
 	background-color: #606060;
 	color: whitesmoke;
 	font-weight: bold;
@@ -215,7 +208,7 @@ export default {
 	width: 35%;
 }
 
-#tables input {
+#tableView input {
 	width: 100%;
 	outline: none;
 	background-color: whitesmoke;
@@ -224,7 +217,7 @@ export default {
 	padding-left: 12px;
 }
 
-#tables #tables textarea {
+#tableView #tableView textarea {
 	background-color: whitesmoke;
 	outline: none;
 	border: none;
@@ -240,7 +233,7 @@ export default {
 	margin: 3px;
 }
 
-#tables .login {
+#tableView .login {
 	margin-top: 25px;
 	margin-right: 10px;
 	text-align: right;
