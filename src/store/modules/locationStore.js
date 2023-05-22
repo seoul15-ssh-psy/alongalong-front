1ab30d2c48fb51e51583687fb0e6123ecddee46f
soun997 <soun997@gmail.com>
2023-05-22 13:40:53 +0900

    fix: 비동기 메소드 흐름 제어, Promise를 반환하도록

--- src/store/modules/locationStore.js
+++ src/store/modules/locationStore.js
@@ -21,33 +21,39 @@ const locationStore = {
       state.currentRegion = region
     },
     SET_ATTRACTION_INFO_LIST: (state, list) => {
+      console.log('list modified')
       state.attractionInfoList = []
       state.attractionInfoList.push(...list)
     }
   },
   actions: {
     async callCurrentLocation({ commit }) {
-      if (!navigator.geolocation) {
-        console.warn('위치 정보를 찾을 수 없습니다.')
-      } else {
-        navigator.geolocation.getCurrentPosition(
-          result => {
-            //console.log(result.coords)
-            commit('SET_CURRENT_LOCATION', {
-              longitude: result.coords.longitude,
-              latitude: result.coords.latitude
-            })
-          },
-          error => {
-            console.warn('위치 정보를 찾을 수 없습니다. ' + error)
-          }
-        )
-      }
+      return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+          console.warn('위치 정보를 찾을 수 없습니다.')
+        } else {
+          navigator.geolocation.getCurrentPosition(
+            result => {
+              resolve(result)
+              console.log('callCurrentLocation')
+              commit('SET_CURRENT_LOCATION', {
+                longitude: result.coords.longitude,
+                latitude: result.coords.latitude
+              })
+            },
+            error => {
+              reject(error)
+              console.warn('위치 정보를 찾을 수 없습니다. ' + error)
+            }
+          )
+        }
+      })
     },
     async callLocation2Region({ commit }, locationInfo) {
       await location2Region(
         locationInfo,
         result => {
+          console.log('callLocation2Region')
           commit('SET_CURRENT_REGION', result.data.documents[0])
         },
         error => {
@@ -59,6 +65,7 @@ const locationStore = {
       await getLocationBasedList(
         locationInfo,
         result => {
+          console.log('callLocationBasedList')
           const list = result.data.response.body.items.item
           commit('SET_ATTRACTION_INFO_LIST', list)
         },
