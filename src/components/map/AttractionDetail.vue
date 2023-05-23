e0d4da350aba79935d1ec747a4fdd755fafe0ec1
sypark <soun997@gmail.com>
2023-05-24 03:59:46 +0900

    feat: 관광 타입 별 상세 정보 출력 기능 구현 완료

--- src/components/map/AttractionDetail.vue
+++ src/components/map/AttractionDetail.vue
@@ -9,9 +9,9 @@
       />
     </div>
     <!-- 관광지 정보 -->
-    <div style="height: 15%">
+    <div class="q-my-md" style="height: 15%">
       <!-- 관광지 타입, 관광지 명 -->
-      <div class="row q-ma-md">
+      <div class="row q-mx-md">
         <div class="text-subtitle1 text-grey">
           {{
             this.contentType[attraction.contenttypeid] +
@@ -20,18 +20,32 @@
           }}
         </div>
         <div
-          class="text-h5 text-bold text-black"
+          class="ellipsis-component text-h5 text-bold text-black"
           style="text-decoration: underline"
         >
           {{ this.attraction.title }}
         </div>
       </div>
       <!-- 관광지 주소 정보 -->
-      <div class="row q-mx-md">
-        <div class="row text-subtitle1 text-bold text-grey-8">
-          {{ '주소: ' + this.attraction.addr1 }}
+      <div class="row q-ma-md">
+        <div
+          class="ellipsis-component text-subtitle1 text-bold text-black items-center"
+        >
+          <q-icon
+            class="q-mr-xs"
+            name="location_on"
+            color="grey-7"
+            size="17px"
+          ></q-icon>
+          {{ this.attraction.addr1 }}
         </div>
-        <div class="row text-subtitle2 text-grey-8 q-mb-sm">
+        <div class="row text-subtitle2 text-black q-mb-sm items-center">
+          <q-icon
+            class="q-mr-sm"
+            name="subway"
+            color="grey-7"
+            size="17px"
+          ></q-icon>
           {{
             this.subwayStation.place_name +
             '에서 ' +
@@ -41,25 +55,24 @@
         </div>
       </div>
     </div>
-    <div class="full-width q-my-sm">
+    <div class="full-width q-mt-lg q-mb-md">
       <q-separator color="grey-4" />
     </div>
     <div style="height: 45%">
-      <q-scroll-area style="height: 100%">
+      <q-scroll-area style="height: 95%">
         <!-- 관광지 상세 정보 -->
         <div class="q-mx-md">
           <template v-for="(item, index) in this.detailContents" :key="index">
-            <div class="full-width q-my-mx">
-              <div class="text-subtitle1 text-bold">{{ item[0] }}</div>
+            <div class="full-width">
+              <div class="text-subtitle1 text-bold" v-html="item[0]"></div>
               <q-separator color="grey-4" />
             </div>
-            <div class="full-width q-my-md">
-              <div
-                class="text-subtitle2"
-                v-for="(data, index) in item[1]"
-                :key="index"
-              >
-                {{ data }}
+            <div class="full-width q-mt-sm q-mb-lg">
+              <div v-for="(data, index) in item[1]" :key="index">
+                <div
+                  class="text-subtitle2"
+                  v-html="data ? data : this.emptyData"
+                ></div>
               </div>
             </div>
           </template>
@@ -85,7 +98,8 @@ export default {
   data() {
     return {
       contentType: contentTypeId,
-      detailContents: []
+      detailContents: [],
+      emptyData: '<span class="text-grey-6">데이터가 존재하지 않습니다.</span>'
     }
   },
   computed: {
@@ -98,20 +112,66 @@ export default {
   watch: {
     modalContentsDetail(attractionDetail) {
       console.log(attractionDetail)
-      if (attractionDetail.contenttypeid == 39) {
+      if (attractionDetail.contenttypeid == 12) {
         this.detailContents = [
-          ['운영시간', [attractionDetail.opentimefood]],
-          ['휴무일', [attractionDetail.restdatefood]],
-          ['연락처', [attractionDetail.infocenterfood]],
+          ['운영시간', [attractionDetail.usetime]],
+          ['휴무일', [attractionDetail.restdate]],
+          ['문의', [attractionDetail.infocenter]],
+          ['주차시설', [attractionDetail.parking]],
           [
             '정보',
             [
-              '주차: ' + attractionDetail.parkingfood,
-              '포장: ' + attractionDetail.packing,
-              '예약: ' + attractionDetail.reservationfood
+              '유모차제공 : ' +
+                this.makeNotEmptyData(attractionDetail.chkbabycarriage),
+              '애완동물동반가능 : ' +
+                this.makeNotEmptyData(attractionDetail.chkpet)
+            ]
+          ]
+        ]
+      } else if (attractionDetail.contenttypeid == 14) {
+        this.detailContents = [
+          ['운영시간', [attractionDetail.usetimeculture]],
+          ['휴무일', [attractionDetail.restdateculture]],
+          ['입장료/이용요금', [attractionDetail.usefee]],
+          ['할인정보', [attractionDetail.discountinfo]],
+          ['문의', [attractionDetail.infocenterculture]],
+          [
+            '주차',
+            [attractionDetail.parkingculture + attractionDetail.parkingfee]
+          ],
+          [
+            '정보',
+            [
+              '유모차제공 : ' +
+                this.makeNotEmptyData(attractionDetail.chkbabycarriageculture),
+              '애완동물동반가능 : ' +
+                this.makeNotEmptyData(attractionDetail.chkpetculture)
+            ]
+          ]
+        ]
+      } else if (attractionDetail.contenttypeid == 28) {
+        this.detailContents = [
+          ['운영시간', [attractionDetail.usetimeleports]],
+          ['휴무일', [attractionDetail.restdateleports]],
+          ['입장료/이용요금', [attractionDetail.usefeeleports]],
+          ['예약사이트', [attractionDetail.reservation]],
+          ['문의', [attractionDetail.infocenterleports]],
+          [
+            '주차',
+            [
+              attractionDetail.parkingleports +
+                attractionDetail.parkingfeeleports
             ]
           ],
-          ['메뉴', [attractionDetail.treatmenu]]
+          [
+            '정보',
+            [
+              '유모차제공 :' +
+                this.makeNotEmptyData(attractionDetail.chkbabycarriageleports),
+              '애완동물동반가능 :' +
+                this.makeNotEmptyData(attractionDetail.chkpetleports)
+            ]
+          ]
         ]
       } else if (attractionDetail.contenttypeid == 32) {
         this.detailContents = [
@@ -119,24 +179,51 @@ export default {
           ['체크아웃', [attractionDetail.checkouttime]],
           ['편의시설', [attractionDetail.subfacility]],
           ['식당', [attractionDetail.foodplace]],
+          ['예약사이트', [attractionDetail.reservationurl]],
+          ['예약문의', [attractionDetail.reservationlodging]],
           [
             '정보',
             [
               '취사: ' + attractionDetail.chkcooking,
               '주차: ' + attractionDetail.infocenterlodging
             ]
+          ]
+        ]
+      } else if (attractionDetail.contenttypeid == 39) {
+        this.detailContents = [
+          ['운영시간', [attractionDetail.opentimefood]],
+          ['휴무일', [attractionDetail.restdatefood]],
+          ['문의', [attractionDetail.infocenterfood]],
+          [
+            '정보',
+            [
+              '주차 : ' + this.makeNotEmptyData(attractionDetail.parkingfood),
+              '포장 : ' + this.makeNotEmptyData(attractionDetail.packing),
+              '예약 : ' +
+                this.makeNotEmptyData(attractionDetail.reservationfood)
+            ]
           ],
-          ['예약사이트', [attractionDetail.reservationurl]],
-          ['예약 문의', [attractionDetail.reservationlodging]]
+          ['메뉴', [attractionDetail.treatmenu]]
         ]
       }
+      console.log(this.detailContents)
     }
   },
   methods: {
     ...mapMutations(locationStore, ['SET_IS_DETAIL_MODAL_UPDATED']),
-    ...mapActions(locationStore, ['callClosestSubwayStation'])
+    ...mapActions(locationStore, ['callClosestSubwayStation']),
+    makeNotEmptyData(data) {
+      return data ? data : this.emptyData
+    }
   }
 }
 </script>
 
-<style></style>
+<style>
+.ellipsis-component {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  max-width: 100%;
+  white-space: nowrap;
+}
+</style>
